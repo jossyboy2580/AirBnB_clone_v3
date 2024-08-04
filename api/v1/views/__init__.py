e278fb01652ec8ef6c5018c603d2c6d3f3adf98c
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-08-04 17:57:52 +0100

    found another technique for the circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,13 +3,9 @@
 an init file for our views
 """
 from flask import Blueprint
-from api.v1.views.index import *
 
 
 app_views = Blueprint('app_views', __name__)
 
 
-@app_views.route('/status')
-def api_status():
-    """return the status of the api"""
-    return jsonify({"status": "OK"})
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,3 +3,10 @@
 all the index for our app
 """
 from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def api_status():
+    """return the status of the api"""
+    return jsonify({"status": "OK"})
