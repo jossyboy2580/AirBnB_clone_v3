786d4a100d3351fe1a3a9eb17f38b7881e1f9d9d
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-08-04 19:04:38 +0100

    getting our 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 The entry point of our flask app
 """
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -17,6 +17,11 @@ app.register_blueprint(app_views, url_prefix='/api/v1/')
 def close_storage(exception):
     """This method closes the storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"error": "Not Found"}), 404
 
 
 if __name__ == '__main__':
