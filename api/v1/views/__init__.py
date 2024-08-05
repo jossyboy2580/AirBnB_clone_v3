a5585d01258a3df959a584ae52c8cc9a39651499
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-08-05 06:35:48 +0100

    added a strict-slashes set to false to the states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,7 @@ an init file for our views
 from flask import Blueprint
 
 
-app_views = Blueprint('app_views', __name__, strict_slashes=False)
+app_views = Blueprint('app_views', __name__)
 
 
 from api.v1.views.index import *
